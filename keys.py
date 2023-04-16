d3fb98a02e9db1c1d4397c58ab4bf58f683b4162
revantkumargupta <125722896+revantkumargupta@users.noreply.github.com>
2023-04-16 19:33:04 +0545

    Update keys.py

--- keys.py
+++ keys.py
@@ -4,4 +4,4 @@ bot_token = ""
 
 service_file = 'deezer-383700-cb9023b1ae29.json'
 db_url = ""
-deezer_api = 'a04970b61e3c88f23f8fd40fe75798a9810dbf1768dbc47a5a1c6b1430d86d28379167e7dc26992e1467fba868b31a67974d5a7ba053bf0195fd9464bb8c7d1e28ab5de7cac853d1381a4dd9da875f9bdccc25b2f44e5ba18c2f0391d82883bf'
+deezer_api = ''
