68cc9b3e55f7663d2bc868e1a6a4a8875653bfdd
revantkumargupta <125722896+revantkumargupta@users.noreply.github.com>
2023-04-18 07:47:36 +0545

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ links = MongoClient(keys.db_url)["deezer_bot"]["links"]
 
 @bot.on_message(filters.command("start"))
 async def start_message(client, message):
-    await message.reply_text("Hello")
+    await message.reply_text("It downloads songs from Deezer and currently supports links from Deezer and Spotify")
 
 
 @bot.on_message(filters.regex(r"^https?:\/\/(?:www\.)?deezer\.com\/([a-z]*\/)?album\/(\d+)\/?$"))
